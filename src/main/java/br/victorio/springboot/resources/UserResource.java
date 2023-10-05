588335e0f14a57b7946b9cb417407a2e5f5176ca
joao.victorio <joaovitorvictorio@gmail.com>
2023-10-05 20:58:48 -0300

    created user entity and resource

---
+++ src/main/java/br/victorio/springboot/resources/UserResource.java
@@ -0,0 +1,20 @@
+package br.victorio.springboot.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.victorio.springboot.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "João", "joao@teste.com", "996967070", "123654");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
