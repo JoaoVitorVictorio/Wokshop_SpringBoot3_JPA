8382b03430c72cc73d27f169239f0c9f8a64cf01
joao.victorio <joaovitorvictorio@gmail.com>
2023-10-18 19:46:40 -0300

    Implemented one-to-one payment entity and membership

--- src/main/java/br/victorio/springboot/config/TestConfig.java
+++ src/main/java/br/victorio/springboot/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.victorio.springboot.entities.Category;
 import br.victorio.springboot.entities.Order;
 import br.victorio.springboot.entities.OrderItem;
+import br.victorio.springboot.entities.Payment;
 import br.victorio.springboot.entities.Product;
 import br.victorio.springboot.entities.User;
 import br.victorio.springboot.entities.enums.OrderStatus;
@@ -24,70 +25,82 @@ import br.victorio.springboot.repositories.UserRepository;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 
-    // Repositórios injetados
-    @Autowired
-    private UserRepository userRepository;
+	// Repositórios injetados
+	@Autowired
+	private UserRepository userRepository;
 
-    @Autowired
-    private OrderRepository orderRepository;
+	@Autowired
+	private OrderRepository orderRepository;
 
-    @Autowired
-    private CategoryRepository categoryRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 
-    @Autowired
-    private ProductRepository productRepository;
-    
-    @Autowired
-    private OrderItemRepository orderItemRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
-    @Override
-    public void run(String... args) throws Exception {
-        // Criação das categorias
-        Category cat1 = new Category(null, "Electronics");
-        Category cat2 = new Category(null, "Books");
-        Category cat3 = new Category(null, "Computers");
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
-        // Criação de usuários
-        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+	@Override
+	public void run(String... args) throws Exception {
+		// Criação das categorias
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
 
-        // Criação de pedidos
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+		// Salvando categorias no banco de dados
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
-        // Criação de produtos
-        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
-        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
-        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
-        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
-        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-        
-        // Criação de itens de pedido
-        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
-        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
-        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
-        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		// Criação de usuários
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        // Salvando entidades no banco de dados
-        userRepository.saveAll(Arrays.asList(u1, u2));
-        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-        
+		// Salvando usuários no banco de dados
+		userRepository.saveAll(Arrays.asList(u1, u2));
 
-        // Associando produtos às categorias
-        p1.getCategories().add(cat2);
-        p2.getCategories().add(cat1);
-        p2.getCategories().add(cat3);
-        p3.getCategories().add(cat3);
-        p4.getCategories().add(cat3);
-        p5.getCategories().add(cat2);
+		// Criação de pedidos
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
-        // Salvando produtos atualizados no banco de dados
-        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-        
-    }
+		// Salvando pedidos no banco de dados
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
+		// Criação de produtos
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+		// Salvando produtos no banco de dados
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		// Criação de itens de pedido
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+		// Salvando itens de pedido no banco de dados
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		// Associando produtos às categorias
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+
+		// Salvando produtos atualizados no banco de dados
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		// Criação dos pagamentos
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		// Salvando o pagamento no banco de dados
+		orderRepository.save(o1);
+	}
 }
