a94408595de0b11246d721873d5d8bb2b30214c5
joao.victorio <joaovitorvictorio@gmail.com>
2023-10-23 20:10:38 -0300

    Add exception handling - delete and update

--- src/main/java/br/victorio/springboot/repositories/OrderItemRepository.java
+++ src/main/java/br/victorio/springboot/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package br.victorio.springboot.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.victorio.springboot.entities.OrderItem;
+import br.victorio.springboot.entities.pk.OrderItemPk;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPk> {
 
 }
--- src/main/java/br/victorio/springboot/services/UserService.java
+++ src/main/java/br/victorio/springboot/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.victorio.springboot.entities.User;
 import br.victorio.springboot.repositories.UserRepository;
+import br.victorio.springboot.services.exceptions.DatabaseException;
 import br.victorio.springboot.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 
@@ -25,15 +28,21 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-
+	
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-
+	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-
+	
 	public User update(Long id, User obj) {
 		try {
 			User entity = repository.getReferenceById(id);
@@ -41,7 +50,7 @@ public class UserService {
 			return repository.save(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException(id);
-		}
+		}	
 	}
 
 	private void updateData(User entity, User obj) {
